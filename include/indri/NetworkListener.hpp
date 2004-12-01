9882b8b081664754160db86d91527847d77914e6
strohman <strohman@14020d50-3050-45fe-8e0c-5ed7005ca764>
2004-12-01 19:25:11 +0000

    Added a throw when NetworkListener can't bind to a port.  Also fixed an issue regarding BIG_ENDIAN; now we check for BIG_ENDIAN == BYTE_ORDER instead of checking for BIG_ENDIAN to be defined.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@32 14020d50-3050-45fe-8e0c-5ed7005ca764

--- include/indri/NetworkListener.hpp
+++ include/indri/NetworkListener.hpp
@@ -68,6 +68,8 @@ public:
   }
 
   bool listen( unsigned int port ) {
+    int result;
+ 
     lemur_compat::initializeNetwork();
     _socket = ::socket( AF_INET, SOCK_STREAM, 0 );
 
@@ -77,13 +79,17 @@ public:
     sa.sin_family = AF_INET;
     memset( &sa.sin_zero, 0, sizeof sa.sin_zero );
 
-    ::bind( _socket, (const sockaddr*) &sa, sizeof sa );
-    int result = ::listen( _socket, 8 );
+    result = ::bind( _socket, (const sockaddr*) &sa, sizeof sa );
+    if( result ) {
+      close();
+      LEMUR_THROW( LEMUR_IO_ERROR, "Wasn't able to bind port " + i64_to_string(port) );
+    }
+  
+    result = ::listen( _socket, 8 );
 
     if( result ) {
       close();
       LEMUR_THROW( LEMUR_IO_ERROR, "Wasn't able to listen on port " + i64_to_string(port) );
-      return false;
     }
 
     return true;
