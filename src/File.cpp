9220dd36fc29108091530fe85cf9374ef5122a3b
strohman <strohman@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-05-12 21:42:51 +0000

    Potential fix for problem where SequentialWriteBuffer doesn't work right on files that already contain data.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@456 14020d50-3050-45fe-8e0c-5ed7005ca764

--- src/File.cpp
+++ src/File.cpp
@@ -220,6 +220,9 @@ void indri::file::File::close() {
 
 UINT64 indri::file::File::size() {
 #ifdef WIN32
+  if( _handle == INVALID_HANDLE_VALUE )
+    return 0;
+
   LARGE_INTEGER length;
   BOOL result = ::GetFileSizeEx( _handle, &length );
 
@@ -229,6 +232,10 @@ UINT64 indri::file::File::size() {
   return length.QuadPart;
 #else // POSIX
   struct stat stats;
+  
+  if( _handle == -1 )
+    return 0;
+
   fstat( _handle, &stats );
   return stats.st_size;
 #endif
