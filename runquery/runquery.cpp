36312776e943cd31a66e8e2284be98e3aa08932a
dfisher <dfisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-07-27 14:05:13 +0000

    Fix queryOffset parameter processing to get the value from the top level parameters, not the query slice.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@655 14020d50-3050-45fe-8e0c-5ed7005ca764

--- runquery/runquery.cpp
+++ runquery/runquery.cpp
@@ -411,8 +411,8 @@ public:
   }
 };
 
-void push_queue( std::queue< query_t* >& q, indri::api::Parameters& queries ) {
-  int queryOffset = queries.get( "queryOffset", 0 );
+void push_queue( std::queue< query_t* >& q, indri::api::Parameters& queries,
+                 int queryOffset ) {
 
   for( int i=0; i<queries.size(); i++ ) {
     int queryNumber;
@@ -453,7 +453,8 @@ int main(int argc, char * argv[]) {
 
     // push all queries onto a queue
     indri::api::Parameters parameterQueries = param[ "query" ];
-    push_queue( queries, parameterQueries );
+    int queryOffset = param.get( "queryOffset", 0 );
+    push_queue( queries, parameterQueries, queryOffset );
     int queryCount = queries.size();
 
     // launch threads
