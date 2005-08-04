f3a61d97f1e3e0a071d02704d269dc241c80f44b
dfisher <dfisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2005-08-04 20:11:56 +0000

    Check for non-empty matches when pruning candidate extents. Bugzilla #38
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@687 14020d50-3050-45fe-8e0c-5ed7005ca764

--- src/ContextCountAccumulator.cpp
+++ src/ContextCountAccumulator.cpp
@@ -80,7 +80,7 @@ void indri::infnet::ContextCountAccumulator::evaluate( int documentID, int docum
     const indri::utility::greedy_vector<indri::index::Extent>& matches = _matches->extents();
     const indri::utility::greedy_vector<indri::index::Extent>& extents = _context->extents();
     unsigned int ex = 0;
-    
+
     for( unsigned int i=0; i<matches.size() && ex < extents.size(); i++ ) {
       // find a context extent that might possibly contain this match
       // here we're relying on the following invariants: 
@@ -93,12 +93,17 @@ void indri::infnet::ContextCountAccumulator::evaluate( int documentID, int docum
       //      same position, the largest end position comes first
       //      (e.g. [1,10] comes before [1,4])
       // Therefore, if a match [a,b] is in any extent, it will be
-      //   in the first one [c,d] such that d>=a.
+      //   in the first one [c,d] such that d>a.or d=a if a=b.
       // Proof is by contradiction: if the match is in a context extent,
       //   but it's not the first one such that d>=a, then that context
       //   extent must overlap the first extent such that d>=a (which
       //   is not allowed).
-      while( extents[ex].end < matches[i].begin ) {
+
+      while( extents[ex].end < matches[i].begin
+             // addresses don's issue in bugzilla #38
+             || ( extents[ex].end == matches[i].begin && 
+                  matches[i].end > matches[i].begin ) ) {
+
         ex++;
 
         if( ex >= extents.size() ) break;
