9f53d395d0fc81f7748a6730128083790974db24
dfisher <dfisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2006-06-07 14:29:14 +0000

    Add dist target to build combined indri + contrib libs as a single indri.lib for windows install.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@881 14020d50-3050-45fe-8e0c-5ed7005ca764

---
+++ dist/dist.cpp
@@ -0,0 +1,16 @@
+/*==========================================================================
+ * Copyright (c) 2006 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+*/
+/*
+        Force aggregation of the component libraries into a unified indri.lib.
+*/
+#ifdef WIN32
+#pragma comment (lib, "indri.lib")
+#endif
