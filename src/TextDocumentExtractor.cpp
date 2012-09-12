fc11225e08195fd5c50de89d8af22d00e4543527
david_fisher <david_fisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2012-09-12 15:38:21 +0000

    Insert filename as docno when no docno element is given.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@2526 14020d50-3050-45fe-8e0c-5ed7005ca764

--- src/TextDocumentExtractor.cpp
+++ src/TextDocumentExtractor.cpp
@@ -42,14 +42,14 @@ indri::parse::UnparsedDocument* indri::parse::TextDocumentExtractor::nextDocumen
   pair.valueLength = _filename.length()+1;
   pair.key = "path";
   _document.metadata.push_back( pair );
-  /*
+
   _docnostring.assign(_filename.c_str() );
   cleanDocno();
   pair.value = _docnostring.c_str();
   pair.valueLength = _docnostring.length()+1;
   pair.key = "docno";
   _document.metadata.push_back( pair );
-  */
+
   pair.key = "filetype";
   pair.value = (void*) "TEXT";
   pair.valueLength = 5;
