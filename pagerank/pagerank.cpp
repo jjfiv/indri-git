bdaf27bf0b3074d46c49906aa8221126e6e0936a
david_fisher <david_fisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2013-05-09 14:53:23 +0000

    Add an option to convert galago pagerank score output (in score sorted order) to a log probability file suitable for input to makeprior, using the indri binning for pagerank scores.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@2566 14020d50-3050-45fe-8e0c-5ed7005ca764

--- pagerank/pagerank.cpp
+++ pagerank/pagerank.cpp
@@ -67,11 +67,67 @@ This data file is suitable for input to the makeprior application.</dd>
 #include "indri/LocalQueryServer.hpp"
 #include "lemur/Exception.hpp"
 #include "indri/PageRank.hpp"
+#include <strings.h>
+#include <iostream>
 
 void require_parameter( const char* name, indri::api::Parameters& p ) {
   if( !p.exists( name ) ) {
     LEMUR_THROW( LEMUR_MISSING_PARAMETER_ERROR, "Must specify a " + name + " parameter." );
   }
+}
+
+
+/** convert galago pagerank score (scoreOrder sorted) to log probability
+    for indri makeprior input
+**/
+
+void convertPriors( const std::string& dest, const std::string &priorFile, 
+                  int numDocs ) {
+  double _intToProb[11] = {log(2.21916E-10), 
+                           log(2.21916E-10),
+                           log(5.61952E-10),
+                           log(1.94786E-09),
+                           log(3.62742E-09), 
+                           log(1.00745E-08),
+                           log(1.97616E-08),
+                           log(3.46944E-08),
+                           log(4.39146E-08),
+                           log(1.74226E-07),
+                           log(2.28983E-07) };
+
+  std::ofstream out(priorFile.c_str(), std::ios::out | std::ios::binary);
+  out.setf( std::ios_base::fixed );
+  out.precision( 13 );
+  std::vector<indri::parse::pagerank> pageranks;
+  // read in the values, input must be in score sorted order.
+  std::ifstream in( dest.c_str(), std::ios::in | std::ios::binary );
+  indri::parse::pagerank p;
+  int max_pr = 10;
+  int rank = max_pr;
+  double B = pow((numDocs + 1.0),(1.0/max_pr));
+  int baseNum = int(B - 1);
+  int num = baseNum;
+
+  while( in.good() && !in.eof() ) {
+    in >> p.doc ;
+    in >> p.val ;
+    if (in.eof())
+      break;
+    // bin the values to integer page ranks
+    if (num > 0) {
+      p.int_val = rank;
+      num--;
+    }
+    if (num == 0) {
+      baseNum = int(ceil(B*baseNum));
+      num = baseNum;
+      if (rank > 1) rank--; // don't allow rank 0
+    }
+    // assign log probabilities to the bins.
+    out << p.doc << " " << _intToProb[p.int_val]<< std::endl;
+  }
+  in.close();
+  out.close();
 }
 
 int main( int argc, char** argv ) {
@@ -82,6 +138,19 @@ int main( int argc, char** argv ) {
     if( parameters.get( "version", false ) ) {
       std::cout << INDRI_DISTRIBUTION << std::endl;
     }
+
+    if( parameters.get( "convert", false ) ) {
+      // converting
+      require_parameter( "input", parameters );
+      require_parameter( "output", parameters );
+      require_parameter( "numDocs", parameters );
+      std::string galagoScore = parameters["input"];
+      std::string outputFile = parameters["output"];
+      int numDocs = parameters["numDocs"];
+      convertPriors(galagoScore, outputFile, numDocs);
+      exit (0);
+    }
+    
     require_parameter( "corpus", parameters );
     require_parameter( "links", parameters );
     require_parameter( "output", parameters );
