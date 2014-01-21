eb9f3e6e1a0f7cc7a9e90003f480df79ac81aabd
david_fisher <david_fisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2014-01-21 14:48:11 +0000

    Use the external symbol for std::ios::openmode instead of the GCC internal symbol. Enables clang (OSX) compilation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@2621 14020d50-3050-45fe-8e0c-5ed7005ca764

--- contrib/lemur/include/lemur/lemur-compat.hpp
+++ contrib/lemur/include/lemur/lemur-compat.hpp
@@ -106,8 +106,8 @@ inline int remove( const char* fileName ) {
 }
 
 #ifdef LEMUR_USES_ENUM_OPENMODE
-inline std::_Ios_Openmode ios_mode_cast( int mode ) {
-  return std::_Ios_Openmode(mode);
+  inline std::ios::openmode ios_mode_cast( int mode ) {
+  return std::ios::openmode(mode);
 }
 #else
 inline int ios_mode_cast( int mode ) {
