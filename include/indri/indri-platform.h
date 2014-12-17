8149d14787f7bb2f5cbf5478c9155142346ab14b
david_fisher <david_fisher@14020d50-3050-45fe-8e0c-5ed7005ca764>
2014-12-17 16:42:14 +0000

    tag and release
    git-svn-id: svn+ssh://svn.code.sf.net/p/lemur/code/indri/trunk@2636 14020d50-3050-45fe-8e0c-5ed7005ca764

--- include/indri/indri-platform.h
+++ include/indri/indri-platform.h
@@ -28,7 +28,7 @@
 #include "lemur/lemur-platform.h"
 
 #define INDRID_PORT ( 16743 )
-#define INDRI_DISTRIBUTION "Indri development release 5.7"
+#define INDRI_DISTRIBUTION "Indri development release 5.8"
 
 #endif /* INDRI_PLATFORM_H */
 
